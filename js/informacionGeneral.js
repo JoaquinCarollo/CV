f591f2233b7e0e475947adcab34ce58563bbff66
JoaquinCarollo <carollojoaquin@gmail.com>
2024-09-06 18:42:24 -0300

    Diseño Responsive Listo

---
+++ js/informacionGeneral.js
@@ -0,0 +1,34 @@
+/**      <div class="iconos">
+        <div>
+          <img
+            src="https://cdn-icons-png.freepik.com/256/455/455705.png?semt=ais_hybrid"
+          /><span>11-2315-0674</span>
+        </div>
+        <div>
+          <img
+            src="https://cdn-icons-png.flaticon.com/256/646/646094.png"
+          /><span>carollojoaquin@gmail.com</span>
+        </div>
+        <div>
+          <img
+            src="https://icons.iconarchive.com/icons/steve/zondicons/512/Location-icon.png"
+          /><span>Villa Crespo, CABA</span>
+        </div>
+      </div> */
+const iconos = document.createElement("div");
+iconos.className = "iconos";
+iconos.innerHTML = `<div>
+                    <img
+                      src="https://cdn-icons-png.freepik.com/256/455/455705.png?semt=ais_hybrid"
+                    /><span>11-2315-0674</span>
+                  </div>
+                  <div>
+                    <img
+                      src="https://cdn-icons-png.flaticon.com/256/646/646094.png"
+                    /><span>carollojoaquin@gmail.com</span>
+                  </div>
+                  <div>
+                    <img
+                      src="https://icons.iconarchive.com/icons/steve/zondicons/512/Location-icon.png"
+                    /><span>Villa Crespo, CABA</span>
+                  </div>`;
--- js/llamarInformacion.js
+++ js/llamarInformacion.js
@@ -1,4 +1,11 @@
 const habilidadesBoton = document.getElementById("habilidadesBoton");
+const informacionGeneralBoton = document.getElementById(
+  "informacionGeneralBoton"
+);
+const academicoYCursosBoton = document.getElementById("academicoYCursosBoton");
+const experienciaLaboralBoton = document.getElementById(
+  "experienciaLaboralBoton"
+);
 const volver = document.createElement("button");
 volver.id = "volver";
 volver.innerText = "Volver";
@@ -12,8 +19,32 @@ function mostrarInfo(informacion) {
     informacionDelCV.appendChild(fotoDePerfilYTitulo);
     habilidades.appendChild(volver);
     informacionDelCV.appendChild(habilidades);
+  } else if (informacion === 2) {
+    informacionDelCV.innerHTML = "";
+    informacionDelCV.appendChild(fotoDePerfilYTitulo);
+    iconos.appendChild(volver);
+    informacionDelCV.appendChild(iconos);
+  } else if (informacion === 3) {
+    informacionDelCV.innerHTML = "";
+    informacionDelCV.appendChild(fotoDePerfilYTitulo);
+    academicoYCursos.appendChild(volver);
+    informacionDelCV.appendChild(academicoYCursos);
+  } else if (informacion === 4) {
+    informacionDelCV.innerHTML = "";
+    informacionDelCV.appendChild(fotoDePerfilYTitulo);
+    experienciaLaboral.appendChild(volver);
+    informacionDelCV.appendChild(experienciaLaboral);
   }
 }
+experienciaLaboralBoton.onclick = () => {
+  mostrarInfo(4);
+};
+academicoYCursosBoton.onclick = () => {
+  mostrarInfo(3);
+};
+informacionGeneralBoton.onclick = () => {
+  mostrarInfo(2);
+};
 habilidadesBoton.onclick = () => {
   mostrarInfo(1);
 };
